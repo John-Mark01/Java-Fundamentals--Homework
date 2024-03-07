1f0c4f1176f2d19e6d42fd6a5d39145e806e79c0
John-Mark Iliev <147177515+John-Mark01@users.noreply.github.com>
2024-03-08 00:18:49 +0200

    More Mapping exercises

--- src/Lambda_Stream_AssosiativeArrays_Exercises/CompanyUsers_08.java
+++ src/Lambda_Stream_AssosiativeArrays_Exercises/CompanyUsers_08.java
@@ -1,6 +1,6 @@
 package Lambda_Stream_AssosiativeArrays_Exercises;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class CompanyUsers_08 {
 
@@ -8,6 +8,26 @@ public class CompanyUsers_08 {
 
         Scanner scanner = new Scanner(System.in);
 
+        Map<String, List<String>> employee = new LinkedHashMap<>();
+        String input = scanner.nextLine();
 
+        while (!input.equals("End")) {
+            String company = input.split(" -> ")[0];
+            String id = input.split(" -> ")[1];
+
+
+            employee.putIfAbsent(company, new ArrayList<>());
+            if (!employee.get(company).contains(id)) {
+                employee.get(company).add(id);
+            }
+            input = scanner.nextLine();
+        }
+
+        for (Map.Entry<String, List<String>> entry : employee.entrySet()) {
+
+            System.out.println(entry.getKey());
+            entry.getValue().forEach(id -> System.out.printf("-- %s\n", id));
+
+        }
     }
 }
