c2fd719d7c4ab1d552946f155f406cc848c8c8f7
John-Mark Iliev <147177515+John-Mark01@users.noreply.github.com>
2024-03-16 16:38:10 +0200

    Regular Expressions --> RegEx More exercises

---
+++ src/Regular_Expressions_Exersice/Furniture_01.java
@@ -0,0 +1,13 @@
+package Regular_Expressions_Exersice;
+
+import java.util.Scanner;
+
+public class Furniture_01 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
--- src/Regular_Expressions_Lecture/MatchPhoneNumber_02.java
+++ src/Regular_Expressions_Lecture/MatchPhoneNumber_02.java
@@ -1,24 +1,28 @@
 package Regular_Expressions_Lecture;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class MatchFullName_01 {
+public class MatchPhoneNumber_02 {
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-        String regex = "\\b(?<firstName>[A-Z]{1}[a-z]+) (?<lastName>[A-Z]{1}[a-z]+)\\b";
+        String regex = "(?<countryCode>\\+359+)(?<delimiter>[-\\s]?)(?<areaCode>[2])\\2(?<number>[0-9]{3}\\2[0-9]{4})\\b";
         String input = scanner.nextLine();
-
 
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(input);
 
+        List<String> phoneBook = new LinkedList<>();
         while (matcher.find()) {
-            System.out.print(matcher.group() + " ");
+            phoneBook.add(matcher.group());
         }
+
+        System.out.println(String.join(", ", phoneBook));
     }
 }
