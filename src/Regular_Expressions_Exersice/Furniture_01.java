5e9af5063e000ba9e5b132e23fb1f6231124289f
John-Mark Iliev <147177515+John-Mark01@users.noreply.github.com>
2024-03-16 18:14:23 +0200

    Regular Expressions --> RegEx More exercises

--- src/Regular_Expressions_Exersice/Furniture_01.java
+++ src/Regular_Expressions_Exersice/Furniture_01.java
@@ -1,6 +1,11 @@
 package Regular_Expressions_Exersice;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Furniture_01 {
 
@@ -9,5 +14,31 @@ public class Furniture_01 {
         Scanner scanner = new Scanner(System.in);
 
 
+        String input = scanner.nextLine();
+        String regex = ">>(?<furniture>[A-Za-z]+)<<(?<price>[0-9]+\\.?[0-9]*)!(?<quantity>[0-9]+)";
+        Pattern pattern = Pattern.compile(regex);
+
+
+        double finalSum = 0;
+        List<String> furnitureList = new LinkedList<>();
+
+        while (!input.equals("Purchase")) {
+
+            Matcher matcher = pattern.matcher(input);
+
+            if (matcher.find()) {
+                String furniture = matcher.group("furniture");
+                double price = Double.parseDouble(matcher.group("price"));
+                int quantity = Integer.parseInt(matcher.group("quantity"));
+
+                furnitureList.add(furniture);
+                double sum = price * quantity;
+                finalSum += sum;
+            }
+            input = scanner.nextLine();
+        }
+        System.out.println("Bought furniture:");
+        furnitureList.forEach(System.out::println);
+        System.out.printf("Total money spend: %.2f", finalSum);
     }
 }
---
+++ src/Regular_Expressions_Exersice/SoftUniBarIncome_02.java
@@ -0,0 +1,13 @@
+package Regular_Expressions_Exersice;
+
+import java.util.Scanner;
+
+public class SoftUniBarIncome_02 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
